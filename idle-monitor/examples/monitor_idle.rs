8f0ff2582306f036b967589bc0c531d06a7d856f
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-01-13 00:40:42 +0200

    Simplify

--- idle-monitor/examples/monitor_idle.rs
+++ idle-monitor/examples/monitor_idle.rs
@@ -1,23 +1,18 @@
-use idle_monitor::run_idle_monitor;
 use idle_monitor::IdleNotification;
+use idle_monitor::run_idle_monitor;
 use std::sync::mpsc::channel;
 use std::time::Duration;
-
-enum AppMessages {
-    Something,
-    IdleNotification(IdleNotification),
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Starting idle monitor example, avoid using your mouse and keyboard for 5 seconds...");
 
     // Create a channel for receiving idle notifications
-    let (tx, rx) = channel::<AppMessages>();
+    let (tx, rx) = channel::<IdleNotification>();
 
     // Spawn the idle monitor in a background thread (5 second timeout)
     run_idle_monitor(
         move |i| {
-            tx.send(AppMessages::IdleNotification(i)).unwrap();
+            tx.send(i).unwrap();
         },
         Duration::from_secs(5),
     );
@@ -25,13 +20,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Listen for idle notifications
     for notification in rx {
         match notification {
-            AppMessages::IdleNotification(IdleNotification::Idle) => {
-                println!("💤 User idle detected!");
+            IdleNotification::Idle => {
+                println!("💤 User is idle!");
             }
-            AppMessages::IdleNotification(IdleNotification::Resumed) => {
+            IdleNotification::Resumed => {
                 println!("✅ User activity resumed!");
             }
-            AppMessages::Something => {}
         }
     }
 
